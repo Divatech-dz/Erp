7db955f14da3f8fd8729c81939ecb0ec7c94e9a4
chikoulat <hamidouboum@hotmail.fr>
2024-12-30 13:15:48 +0100

    FEAT: Added Client API

--- lib/axios.ts
+++ lib/axios.ts
@@ -20,6 +20,10 @@ export const BonRetourAPI = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BR_API_URL,
 });
 
+export const ClientAPI = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_CLIENT_API_URL,
+});
+
 export const UserAPI = axios.create({
     baseURL: process.env.NEXT_PUBLIC_USER_API_URL,
 })
