f3059bf6209085ce8bfd8b57f9b8b985293f277a
chikoulat <hamidouboum@hotmail.fr>
2025-01-05 16:42:56 +0100

    FIX: Removed log in listeClient page

--- app/(root)/(links)/(Clients)/listeClients/page.tsx
+++ app/(root)/(links)/(Clients)/listeClients/page.tsx
@@ -27,7 +27,6 @@ function Page() {
 
     const clientsResults = ClientsData?.results;
     const totalPages = ClientsData?.total_pages;
-    console.log(clientsResults)
 
     return (
         <section>
